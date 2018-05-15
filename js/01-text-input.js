b0b14c046ce2e1a5176f63325e387cf0178cb8b4
tphdev <tphdevdrop@gmail.com>
2018-05-15 12:10:00 -0500

    initial code push

--- js/01-text-input.js
+++ js/01-text-input.js
@@ -2,11 +2,46 @@
 // I-a : Capture text value from <input/> element, put in
 //       <p class="output-box">...</p>
 //--------------------------------------------
+document
+  .querySelector('.enter-btn')
+  .addEventListener('click', function outputText(){
+    // select input
+    var userInputDomEl = document.querySelector('.user-input')
+    // capture text value
+    var userInputText = userInputDomEl.value
+
+    var outputBoxDomEl = document.querySelector('.output-box')
+    outputBoxDomEl.innerHTML = userInputText
+
+    // III - Clear the input for the next entry
+    userInputDomEl.value = ''
+
+  })
+
 
 
 // II-a : keyDown <input/> event to 'keep the x out'
 //--------------------------------------------
 
+var userInputDomEl = document.querySelector('.user-input')
+
+userInputDomEl.addEventListener('keydown', function(evtObj){
+  console.log(evtObj)
+  var currentText = evtObj.target.value
+
+  // check for the 'x' key...
+  if(evtObj.keyCode === 88 ){
+    // prevent the character from showing in the input box
+    evtObj.preventDefault()
+  }
+
+  // check for the '6' key...
+  if(evtObj.keyCode === 54 ){
+    // prevent the character from showing in the input box
+    evtObj.preventDefault()
+  }
+
+})
 
 // III-a : Capture text value from <input/> element
 //        on ENTER key and then clear <input>
--- js/02-radio-check-inputs.js
+++ js/02-radio-check-inputs.js
@@ -5,7 +5,23 @@
 // I-a : Listen to 'change' event on checkbox (use specific selector),
 //       put result on DOM
 //--------------------------------------------
+var olderThanCheckboxEl = document.querySelector('.older-than')
 
+olderThanCheckboxEl.addEventListener('change', function putChangeInResultBox(evtObj){
+  var checkBoxEl = evtObj.currentTarget
+  console.log(checkBoxEl)
+  var currentCheckboxBool = checkBoxEl.checked
+
+
+  var resultBoxEl =  document.querySelector('.you-old')
+
+  if(currentCheckboxBool === true){
+    resultBoxEl.textContent = 'Yes You are VERY old...'
+  } else {
+    resultBoxEl.textContent = 'Not so old, or sexy'
+  }
+
+})
 
 // II-b : Listen to change event on
 //--------------------------------------------
